7be136c03abcccba7113d0351616debe7b03ed81
Rahul Ugale <rugale980@gmail.com>
2025-12-16 12:56:59 +0530

    Add files via upload

---
+++ program314.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int Data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+// Call by value functions
+void Display(PNODE first)
+{
+    while (first != NULL)
+    {
+        printf("%d\t", first->Data);
+        first = first->next;
+    }
+    printf("\n");
+}
+
+int Count(PNODE first)
+{
+    return 0;
+}
+
+// Call by Address functions
+void InsertFirst(PPNODE first, int No)
+{
+}
+
+void InsertLast(PPNODE first, int No)
+{
+}
+
+void InsertAtPos(PPNODE first, int No, int pos)
+{
+}
+
+void DeleteFirst(PPNODE first)
+{
+}
+
+void DeleteLast(PPNODE first)
+{
+}
+
+void DeleteAtPos(PPNODE first, int pos)
+{
+}
+
+int main()
+{
+    PNODE head = NULL;
+
+    Display(head);
+
+    Count(head);
+
+    InsertFirst(&head, 11);
+    InsertLast(&head, 21);
+    InsertAtPos(&head, 51, 5);
+
+    DeleteFirst(&head);
+    DeleteLast(&head);
+    DeleteAtPos(&head, 5);
+
+    return 0;
+}
