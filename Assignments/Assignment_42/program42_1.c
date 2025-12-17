9cf063b3361bcd6a2cec117c15203e640f64e6b8
Rahul Ugale <rugale980@gmail.com>
2025-12-17 14:37:35 +0530

    Add files via upload

---
+++ Assignments/Assignment_42/program42_1.c
@@ -0,0 +1,114 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Required header file
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+struct node
+{
+    int Data;
+    struct node *Next;
+};
+
+/*------------------------------------------------------------------------------
+        Old Name                    New Name
+--------------------------------------------------------------------------------
+        struct Node                 NODE
+        struct Node *               PNODE
+        struct Node **              PPNODE
+--------------------------------------------------------------------------------*/
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : InsertFirst
+//  Description   : Used to insert at first position of linked list
+//  Parameters    : Address of first pointer and data of node
+//  Return value  : void
+//  Author        : Rahul Balasaheb Ugale
+//  Date          : 17/12/2025
+//
+////////////////////////////////////////////////////////////////////////////////
+
+void InsertFirst(PPNODE Head, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->Next = NULL;
+    newn->Data = no;
+
+    if (*Head == NULL)
+    {
+        *Head = newn;
+    }
+    else
+    {
+        newn->Next = *Head;
+        *Head = newn;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : SearchFirstOcc
+//  Description   : Used to Find First element in the linked list
+//  Parameters    : first pointer
+//  Return value  : int
+//  Author        : Rahul Balasaheb Ugale
+//  Date          : 17/12/2025
+//
+////////////////////////////////////////////////////////////////////////////////
+
+int SearchFirstOcc(PNODE Head, int no)
+{
+    int Pos = 1;
+    PNODE temp = Head;
+
+    while (temp != NULL)
+    {
+        if (temp->Data == no)
+        {
+            return Pos;
+        }
+        Pos++;
+        temp = temp->Next;
+    }
+    return -1;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Main Entry point application
+//
+////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iRet = 0;
+
+    PNODE First = NULL;
+
+    InsertFirst(&First, 101);
+    InsertFirst(&First, 51);
+    InsertFirst(&First, 21);
+    InsertFirst(&First, 11);
+
+    iRet = SearchFirstOcc(First, 51);
+
+    printf("Position of First element is : %d\n", iRet);
+
+    return 0;
+}
