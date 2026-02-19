b50b52d3b0970c235b30d0bf2b4b734b955852bc
Rahul Ugale <rugale980@gmail.com>
2026-02-19 15:00:54 +0530

    Add files via upload

---
+++ Searching/program751.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+using namespace std;
+
+
+class ArrayX
+{
+    public:
+      int *Arr;
+      int iSize;
+
+      ArrayX(int no)
+      {
+        cout<<"Inside constructor\n";
+        iSize = no;
+        Arr = new int[iSize];
+      }
+
+      ~ArrayX()
+      {
+        cout<<"Inside destructor\n";
+     
+         delete []Arr;
+      }
+
+      void Accept()
+      {
+        int iCnt = 0;
+        cout<<"Enter the elements : \n";
+
+        for(iCnt = 0; iCnt < iSize ; iCnt++)
+        {
+            cin>>Arr[iCnt];
+        }
+      }
+
+      void Display()
+      {
+        int iCnt = 0;
+        cout<<"Elements of array are : \n";
+ 
+        
+        for(iCnt = 0; iCnt < iSize ; iCnt++)
+        {
+            cout<<Arr[iCnt]<<"\t";
+        }
+
+        cout<<"\n";
+
+
+      }
+
+      bool LinearSearch(int iNo)
+      {
+        bool bFlag = false;
+        int i = 0;
+
+        for(i = 0;i <iSize ; i++)
+        {
+          if(Arr[i] == iNo)
+          {
+            bFlag = true;
+            break;
+          }
+        }
+        return bFlag;
+      }
+
+      bool BiDirectionalSearch(int iNo)
+      {
+        bool bFlag = false;
+        int iStart = 0;
+        int iEnd = 0;
+
+        for(iStart = 0 , iEnd = iSize-1; iStart <= iEnd ; iStart++ , iEnd--)
+        {
+          if((Arr[iStart] == iNo)  || (Arr[iEnd] == iNo))
+          {
+            bFlag = true;
+            break;
+          }
+        }
+        return bFlag;
+      }
+
+      
+};
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    cout<<"Enter the number of elements : \n";
+    cin>>iValue;
+
+    ArrayX aobj(iValue);
+    aobj.Accept();
+    aobj.Display();
+
+    if(aobj.LinearSearch(21))
+    {
+      cout<<"element is present using linear\n";
+    }
+    else
+    {
+      cout<<"element is not present\n";
+      
+    }
+
+    if(aobj.BiDirectionalSearch(21))
+    {
+      cout<<"element is present using bidirectional\n";
+    }
+    else
+    {
+      cout<<"element is not present\n";
+      
+    }
+    return 0;
+}
